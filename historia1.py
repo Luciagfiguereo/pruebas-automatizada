749c8779c97e08c00c80b2909aabe07da7e54d9e
Lucia Gabriela Figuereo <Luciagfiguereo@gmail.com>
2022-12-04 15:49:01 -0400

    historia01
    corri las pruebas de la historia de usuario 1

--- historia1.py
+++ historia1.py
@@ -1,5 +1,7 @@
 import HtmlTestRunner
 from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
 import unittest
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -16,28 +18,32 @@ class LoginUsuario_lucia(unittest.TestCase):
         self.s = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=self.s)
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        #chromedriver_autoinstaller.install()
         self.driver.maximize_window()
-        self.driver.implicitly_wait(time_to_wait=10)
+        self.driver.implicitly_wait(time_to_wait=5)
 
     def test_login_page_correct01(self):
-        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin ")
-        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
-        login = self.driver.find_element(By.ID, "btnLogin").click()
+        usuario = self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        password = self.driver.find_element(By.NAME, 'password').send_keys("admin123")
+        login = self.driver.find_element(By.CLASS_NAME, "orangehrm-login-button").click()
+        msj = self.driver.find_element(By.CLASS_NAME, "oxd-topbar-header-breadcrumb")
+        self.assertTrue(msj)
 
-        welcome = self.driver.find_element(By.ID, 'welcome').click()
-
-        print("Prueba Correcta - US 1 |  TC01 | Validar Login Usuario Correcto")
+        print("Prueba Correcta - Historia 01 | Caso de prueba 01 | Validar Login Usuario Correcto")
 
 
 
     def test_login_page_incorrect01(self):
-        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
-        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin12345")
-        login = self.driver.find_element(By.ID, "btnLogin").click()
+        usuario = self.driver.find_element(By.NAME, 'username').send_keys("Admin1")
+        password = self.driver.find_element(By.NAME, 'password').send_keys("admin12345")
+        login = self.driver.find_element(By.CLASS_NAME, "orangehrm-login-button").click()
         #welcome = self.driver.find_element(By.ID, 'welcome').click()
-        msj = self.driver.find_element(By.XPATH, "//span[@id='spanMessage']")
-        self.assertTrue(msj)
-        print("Prueba Correcta - US 1 | TC02 | Validar Login Usuario Incorrecto")
+        msj_error = self.driver.find_element(By.CLASS_NAME, "oxd-alert-content-text")
+        #msj_error = self.driver.find_element(By.XPATH, "//[contains(text(), 'Invalid credentials')]”")
+        self.assertTrue(msj_error)
+        
+        print("Prueba Correcta - Historia 01 | Caso de prueba 02 | Validar Login Usuario Incorrecto")
+
 
 
 
@@ -49,6 +55,7 @@ class LoginUsuario_lucia(unittest.TestCase):
 
 if __name__ == '__main__':
     print("prueba exitosa")
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:\\Users\\Numbers Tech\\Desktop\Python\\tarea4-pruebas\\Reportes'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:\\Users\\Numbers Tech\\Desktop\Python\\pruebas-automatizada\\Reportes'))
+    #unittest.main()
 
 
